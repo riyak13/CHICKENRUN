a0671e840329032b67768c727ac6978e8c7f9065
Riya Kaynat <kaynat.riya@gmail.com>
2017-12-11 14:41:01 +0200

    DB WORKING

--- Assets/Wild/Scripts/db.cs
+++ Assets/Wild/Scripts/db.cs
@@ -9,22 +9,23 @@ public class db : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-		string conn = "URI=file:" + Application.dataPath + "Plugins/Users.s3db"; //Path to database.
+		Debug.Log ("DB script starts");
+		// string conn = "URI=file:" + Application.dataPath + "Plugins/Users.db"; //Path to database.
+		string conn = "URI=file:Users.db"; 
 		IDbConnection dbconn;
 		dbconn = (IDbConnection) new SqliteConnection(conn);
 		dbconn.Open(); //Open connection to the database.
 		IDbCommand dbcmd = dbconn.CreateCommand();
-		string sqlQuery = "SELECT Id,Username,Score" + "FROM UserInfo";
+		string sqlQuery = "SELECT Id,Username,Score FROM UserInfo";
 		dbcmd.CommandText = sqlQuery;
 		IDataReader reader = dbcmd.ExecuteReader();
 		while (reader.Read())
 		{
 			int Id = reader.GetInt32(0);
 			string Username = reader.GetString(1);
-			int Score = reader.GetInt32(3);
+			int Score = reader.GetInt32(2);
 
-			Debug.Log( "Id= "+Id+"  name ="+Username+" Score= "+Score+"" );
+			Debug.Log( "Id= "+Id+"  name ="+Username+" Score= "+Score);
 		}
 		reader.Close();
 		reader = null;
