c81cdff40517fa7868f3d9f96199cd8848d781b3
Riya Kaynat <kaynat.riya@gmail.com>
2017-12-12 00:00:29 +0200

    HIGHSCORE

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssemblyCSharp
+{
+	public class HighScore
+	{
+		public int Id { get; set; }
+		public string Username {get; set;}
+		public int Score { get; set; }
+
+		public HighScore (int Id, int Score, int Username)
+		{
+			this.Score = Score;
+			this.Username = Username;
+			this.Id = Id;
+		}
+	}
+}
+
--- Assets/Scripts/db.cs
+++ Assets/Scripts/db.cs
@@ -6,62 +6,81 @@ using System.Data;
 using System;
 
 public class db : MonoBehaviour {
+	private string conn;
+	private List<HighScore> = new List<HighScore>;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("DB script starts");
 		// string conn = "URI=file:" + Application.dataPath + "Plugins/Users.db"; //Path to database.
 		string conn = "URI=file:Users.db"; 
-		IDbConnection dbconn;
-		dbconn = (IDbConnection) new SqliteConnection(conn);
-		dbconn.Open(); //Open connection to the database.
-		IDbCommand dbcmd = dbconn.CreateCommand();
-		string sqlQuery = "SELECT *FROM UserInfo";
-		//Id,Username,Score 
-		dbcmd.CommandText = sqlQuery;
+		GetScores ();
+	}
 
-		IDataReader reader = dbcmd.ExecuteReader(); 
+	void Update (){ private void InsertScore (string name, int NewScore) {
+			string conn = "URI=file:Users.db"; 
+			IDbConnection dbconn;
+			dbconn = (IDbConnection) new SqliteConnection(conn);
+			dbconn.Open(); 
 
-		//executes commands to reader
+			//Open connection to the database.
 
-		while (reader.Read())
-		{
-			int Id = reader.GetInt32(0);
-			string Username = reader.GetString(1);
-			int Score = reader.GetInt32(2);
+			IDbCommand dbcmd = dbconn.CreateCommand();
+			string sqlQuery = String.Format (
+				"INSERT INTO UserInfo (Id,Username,Score) VALUES (\"{1}\",\"{2}\")",name,NewScore); 
+			dbcmd.CommandText = sqlQuery;
+			dbcmd.ExecuteScalar ();
+			dbconn.Close ();
 
-			Debug.Log( "Id= "+Id+"  name ="+Username+" Score= "+Score);
+			//shows data on console
+
 		}
 
-		//shows data on console
-		reader.Close();
-		reader = null;
-		dbcmd.Dispose();
-		dbcmd = null;
-		dbconn.Close();
-		dbconn = null;
-
-		//closing the reader
-	}
-
-	private void InsertScore (string name, int NewScore) {
-		string conn = "URI=file:Users.db"; 
-		IDbConnection dbconn;
-		dbconn = (IDbConnection) new SqliteConnection(conn);
-		dbconn.Open(); 
-
-		//Open connection to the database.
-
-		IDbCommand dbcmd = dbconn.CreateCommand();
-		string sqlQuery = String.Format (
-			"INSERT INTO UserInfo (Id,Username,Score) VALUES (\"{1}\",\"{2}\")",name,NewScore); 
-		dbcmd.CommandText = sqlQuery;
-		dbcmd.ExecuteScalar ();
-		dbconn.Close ();
-
-		//shows data on console
 
 	}
+		
+			public void GetScores () 
+	{
+		HighScore.Clear (); 
+	string conn = "URI=file:Users.db"; 
+	IDbConnection dbconn;
+	dbconn = (IDbConnection) new SqliteConnection(conn);
+	dbconn.Open(); //Open connection to the database.
+	IDbCommand dbcmd = dbconn.CreateCommand();
+	string sqlQuery = "SELECT *FROM UserInfo";
+	//Id,Username,Score 
+	dbcmd.CommandText = sqlQuery;
+
+	IDataReader reader = dbcmd.ExecuteReader(); 
+
+	//executes commands to reader
+
+	while (reader.Read())
+	{
+		int Id = reader.GetInt32(0);
+		string Username = reader.GetString(1);
+		int Score = reader.GetInt32(2);
+
+			HighScore.Add (
+				new HighScore (reader.GetInt32 (0),
+				reader.GetString (1)
+				reader.GetInt32 (2)));
+				
+
+		Debug.Log( "Id= "+Id+"  name ="+Username+" Score= "+Score);
+	}
+
+	//shows data on console
+	reader.Close();
+	reader = null;
+	dbcmd.Dispose();
+	dbcmd = null;
+	dbconn.Close();
+	dbconn = null;
+
+	//closing the reader
+
+
 
 	private void DeleteScore ( int Id) {
 		string conn = "URI=file:Users.db"; 
@@ -78,7 +97,10 @@ public class db : MonoBehaviour {
 		dbcmd.ExecuteScalar ();
 		dbconn.Close ();
 
+		//deletes ID 
+
 			
 	}
+}
 }
 	
